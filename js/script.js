6095d18e3342fadaf1358f213ca761f6389d02af
Karl Westin <karl.westin@gmail.com>
2012-05-13 15:27:27 +0200

    Step 2: Breaking out the lightbox into a simple class

--- js/script.js
+++ js/script.js
@@ -3,45 +3,49 @@
  * Part of the "refactoring javascript for unit testing" blog post
  */
 
-$(document).ready(function() {
+function Lightbox(boxcontent) {
+    var content = "<div class='js-overlay'></div>" +
+                  "<div class='js-dialog'>" + 
+                      "<a href='#' class='js-close'>Close</a>" + 
+                      "<div class='js-content'>" + 
+                      boxcontent +
+                      "</div>" +
+                  "</div>",
+        width, 
+        height;
 
-    $("#boxlink").bind("click", function(e) {
-        var content = "<div class='js-overlay'></div>" +
-                      "<div class='js-dialog'>" + 
-                          "<a href='#' class='js-close'>Close</a>" + 
-                          "<div class='js-content'>" + 
+    $("body").append(content);
 
-                          // content:
-                          "<img height='331' width='500' src='img/spaceshuttle.jpg' title='Space shuttle on a Plane!'>" +
-                          "<br>Gorgeous photo of the space shuttle by <a href='http://www.flickr.com/photos/deg_io/5702042693/'>flickr user deg_io</a>" + 
+    $(".js-overlay").bind("click", this.closeBox);
+    $(".js-close").bind("click", this.closeBox);
 
-                          "</div>" +
-                      "</div>",
-            width, 
-            height;
+    width = $(".js-content").width();
+    height = $(".js-content").height();
+    $(".js-dialog").width(width)
+                   .height(height)
+                   .css("margin-left", -width/2)
+                   .css("margin-top", -height/2);
 
-        e.preventDefault();
-                      
-        $("body").append(content);
+    $(".js-overlay").hide().fadeIn();
+}
 
-        $(".js-overlay").bind("click", closeBox);
-        $(".js-close").bind("click", closeBox);
+Lightbox.prototype = {
 
-        width = $(".js-content").width();
-        height = $(".js-content").height();
-        $(".js-dialog").width(width)
-                       .height(height)
-                       .css("margin-left", -width/2)
-                       .css("margin-top", -height/2);
-
-        $(".js-overlay").hide().fadeIn();
-    });
-
-    function closeBox(e) {
+    closeBox: function(e) {
         e.preventDefault();
         $(".js-close").unbind("click");
         $(".js-overlay").unbind("click").fadeOut($(".js-overlay").remove.bind($(".js-overlay")));
         $(".js-dialog").remove();
     }
 
+};
+
+$(document).ready(function() {
+    $("#boxlink").bind("click", function(e) {
+        var content = "<img height='331' width='500' src='img/spaceshuttle.jpg' title='Space shuttle on a Plane!'>" +
+                      "<br>Gorgeous photo of the space shuttle by <a href='http://www.flickr.com/photos/deg_io/5702042693/'>flickr user deg_io</a>";
+
+        e.preventDefault();
+        new Lightbox(content);
+    });
 });
